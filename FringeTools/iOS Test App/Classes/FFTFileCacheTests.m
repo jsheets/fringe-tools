d447d5a6fca78aa6166a0ea52086613322aee341
John Sheets <john.sheets@jamfsoftware.com>
2011-01-13 23:31:39 -0600

    Install GHUnit into iOS test app.

---
+++ FringeTools/iOS Test App/Classes/FFTFileCacheTests.m
@@ -0,0 +1,26 @@
+//
+//  FFTFileCacheTests.m
+//  FringeTools-iOS
+//
+//  Created by John Sheets on 1/13/11.
+//  Copyright 2011 MobileMethod, LLC. All rights reserved.
+//
+
+@interface FFTFileCacheTests : GHTestCase {}
+@end
+
+
+@implementation FFTFileCacheTests
+
+- (void)testFailure
+{
+    GHAssertTrue(NO, @"Destined to fail");
+}
+
+- (void)testSuccess
+{
+    GHAssertTrue(YES, @"Destinted for greatness!");
+}
+
+
+@end
